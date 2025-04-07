1db4fe13e43b26ddbde7b443dc12d4f6c228e4cb
Darina Khalina <darina.khalina@namecheap.com>
2025-04-07 09:36:34 +0300

    add .env

--- src/conf/config.py
+++ src/conf/config.py
@@ -1,10 +1,22 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER", "postgres")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "db-hw-8")
+
+if DB_PASSWORD is None:
+    raise ValueError("DB_PASSWORD is not set in environment variables")
 
 
 class Config:
-    DB_URL = f"postgresql+asyncpg://postgres:1239@{DB_HOST}:5432/db-hw-8"
+    DB_URL = (
+        f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
 
 
 config = Config
